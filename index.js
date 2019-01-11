257ee4300098a98cb6b784709bc7b1c4abdcd34a
Luke Plachta <luke.plachta@outlook.com>
2019-01-11 21:48:10 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,115 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function oneTimesTable() {
+    var display = "";
+    for (i = 1; i < 13; i++) {
+        var multiplier = 1;
+        var result = i * 1;
+    display += multiplier + " * " + i + " = " + result + "<br>";    
+    }
+    document.getElementById("outputDivOne").innerHTML = display;
+}
+function twoTimesTable() {
+    var display = "";
+    for (i = 1; i < 13; i++) {
+        var multiplier = 2;
+        var result = i * 2;
+    display += multiplier + " * " + i + " = " + result + "<br>";    
+    }
+    document.getElementById("outputDivTwo").innerHTML = display;
+}
+function threeTimesTable() {
+    var display = "";
+    for (i = 1; i < 13; i++) {
+        var multiplier = 3;
+        var result = i * 3;
+    display += multiplier + " * " + i + " = " + result + "<br>";    
+    }
+    document.getElementById("outputDivThree").innerHTML = display;
+}
+function fourTimesTable() {
+    var display = "";
+    for (i = 1; i < 13; i++) {
+        var multiplier = 4;
+        var result = i * 4;
+    display += multiplier + " * " + i + " = " + result + "<br>";    
+    }
+    document.getElementById("outputDivFour").innerHTML = display;
+}
+function fiveTimesTable() {
+    var display = "";
+    for (i = 1; i < 13; i++) {
+        var multiplier = 5;
+        var result = i * 5;
+    display += multiplier + " * " + i + " = " + result + "<br>";    
+    }
+    document.getElementById("outputDivFive").innerHTML = display;
+}
+function sixTimesTable() {
+    var display = "";
+    for (i = 1; i < 13; i++) {
+        var multiplier = 6;
+        var result = i * 6;
+    display += multiplier + " * " + i + " = " + result + "<br>";    
+    }
+    document.getElementById("outputDivSix").innerHTML = display;
+}
+function sevenTimesTable() {
+    var display = "";
+    for (i = 1; i < 13; i++) {
+        var multiplier = 7;
+        var result = i * 7;
+    display += multiplier + " * " + i + " = " + result + "<br>";    
+    }
+    document.getElementById("outputDivSeven").innerHTML = display;
+}
+function eightTimesTable() {
+    var display = "";
+    for (i = 1; i < 13; i++) {
+        var multiplier = 8;
+        var result = i * 8;
+    display += multiplier + " * " + i + " = " + result + "<br>";    
+    }
+    document.getElementById("outputDivEight").innerHTML = display;
+}
+function nineTimesTable() {
+    var display = "";
+    for (i = 1; i < 13; i++) {
+        var multiplier = 9;
+        var result = i * 9;
+    display += multiplier + " * " + i + " = " + result + "<br>";    
+    }
+    document.getElementById("outputDivNine").innerHTML = display;
+}
+function tenTimesTable() {
+    var display = "";
+    for (i = 1; i < 13; i++) {
+        var multiplier = 10;
+        var result = i * 10;
+    display += multiplier + " * " + i + " = " + result + "<br>";    
+    }
+    document.getElementById("outputDivTen").innerHTML = display;
+}
+function elevenTimesTable() {
+    var display = "";
+    for (i = 1; i < 13; i++) {
+        var multiplier = 11;
+        var result = i * 11;
+    display += multiplier + " * " + i + " = " + result + "<br>";    
+    }
+    document.getElementById("outputDivEleven").innerHTML = display;
+}
+function twelveTimesTable() {
+    var display = "";
+    for (i = 1; i < 13; i++) {
+        var multiplier = 12;
+        var result = i * 12;
+    display += multiplier + " * " + i + " = " + result + "<br>";    
+    }
+    document.getElementById("outputDivTwelve").innerHTML = display;
+}
+
+
